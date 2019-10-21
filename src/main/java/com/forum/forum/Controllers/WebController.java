b260399ad50d4ffbbefc6c0c5b27d0b7716fd421
KenanRico <kenan.hu@mail.utoronto.ca>
2019-10-21 08:53:09 -0400

    k

--- src/main/java/com/forum/forum/Controllers/WebController.java
+++ src/main/java/com/forum/forum/Controllers/WebController.java
@@ -1,6 +1,5 @@
 package com.forum.forum.Controllers;
 
-import com.forum.forum.Database.JsonDatabase;
 import com.forum.forum.Forms.Message;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
@@ -13,10 +12,10 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Controller
 public class WebController {
 
-    JsonDatabase msg_db;
+    MessageDatabase msg_db;
 
     public WebController(){
-        msg_db = new JsonDatabase("Messages.json");
+        msg_db = new MessageDatabase;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
